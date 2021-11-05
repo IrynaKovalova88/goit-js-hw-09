a2c9a3418bcb46be794a6aeb0349283ec1977fd6
IrynaKovalova88 <Avril19882012@gmail.com>
2021-11-06 00:56:03 +0200

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    body: document.querySelector("body"),
+    startButton: document.querySelector("button[data-start]"),
+    stopButton: document.querySelector("button[data-stop]")
+};
+let colorId = null;
 
+refs.startButton.addEventListener('click', onColorStart);
+refs.stopButton.addEventListener('click', onColorStop);
+
+function onColorStart() {
+    colorId = setInterval(bodyColor, 1000);
+    refs.startButton.disabled = true;
+ };
+
+function onColorStop() {
+    clearInterval(colorId);
+    refs.startButton.disabled = false;
+};
+ 
+function bodyColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
